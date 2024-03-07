b972846805051bdc8014ff7bd5411a75c5722e36
Mean Says <oliveirafaelmoreno@gmail.com>
2024-03-07 18:57:52 -0300

    Aula da semana 3 da UBL - Ciencia de dados

---
+++ UBL - Ciencia da computação/Python/lista de excercicios 2/ordenacao.py
@@ -0,0 +1,8 @@
+n1 = int(input(""))
+n2 = int(input(""))
+n3 = int(input(""))
+
+if (n2 > n1 and n3 > n2):
+    print("crescente")
+else:
+    print("não está em ordem crescente")
---
+++ UBL - Ciencia da computação/Python/lista de excercicios 2/paridade.py
@@ -0,0 +1,6 @@
+numero = int(input())
+paridade = numero % 2
+if (int(paridade) == 0):
+    print("par")
+else:
+    print("ímpar")
